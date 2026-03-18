a89a2dfd50ddcbbd8db99583fde0a2136866064a
Matii <matii.espinoza.15@gmail.com>
2026-03-18 17:41:57 -0300

    Dando estilos iniciales

--- reservas/urls.py
+++ reservas/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('buscar/', views.resultados, name='buscar'),
+    path('cabana/<int:cabana_id>/', views.detalle_cabana, name='detalle_cabana'),
 ]
--- reservas/views.py
+++ reservas/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Cabana
 
 def home(request):
-    cabanas = Cabana.objects.all()
+    return render(request, 'reservas/index.html')
+
+def resultados(request):
+    llegada = request.GET.get('llegada', '')
+    salida = request.GET.get('salida', '')
+    personas = request.GET.get('personas', 1)
+    habitaciones = request.GET.get('habitaciones', 1)
+
+    cabanas = Cabana.objects.filter(
+        capacidad_personas__gte=personas,
+        cantidad_habitaciones__gte=habitaciones
+    )
     
-    return render(request, 'reservas/index.html', {'cabanas': cabanas})
+    contexto = {
+        'cabanas': cabanas,
+        'llegada': llegada,
+        'salida': salida,
+        'personas': personas,
+        'habitaciones': habitaciones,
+    }
+    return render(request, 'reservas/resultados.html', contexto)
+
+def detalle_cabana(request, cabana_id):
+    # Buscamos la cabaña por su ID. Si no existe, muestra error 404.
+    cabana = get_object_or_404(Cabana, id=cabana_id)
+    return render(request, 'reservas/detalle.html', {'cabana': cabana})
